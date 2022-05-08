48c6fd6d3adf4dd151d076121210443c2e75c5aa
Kali <darkwing42@gmail.com>
2022-05-08 11:23:20 -0400

    test new combine targets system

--- md2.sh
+++ md2.sh
@@ -142,9 +142,9 @@ sec_targets=$(cat $file | sort | uniq | wc -l)
 main_targets=$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets | cat | grep "^[^#]" | wc -w)
 total_targets=$(expr $sec_targets + $main_targets)
 
-echo -e "\x1b[32m secondary targets" $sec_targets
-echo -e "\x1b[32m main targets" $main_targets
-echo -e "\x1b[32m total" $total_targets
+echo -e "\x1b[32m secondary targets:" $sec_targets
+echo -e "\x1b[32m main targets:" $main_targets
+echo -e "\x1b[32m total:" $total_targets
 sleep 5
 
 # Restart attacks and update targets every 30 minutes
